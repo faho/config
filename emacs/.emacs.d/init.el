c9dbc1d07109d6ed7b6b609764e2af51a0f012e9
Fabian Homborg <FHomborg@gmail.com>
2015-07-02 20:20:54 +0200

    emacs: Remove custom
    One of these days I need to find out how to kill this with fire

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -626,18 +626,6 @@ user."
     (find-file file)))
 ;; C-x f is usually bound to "set-fill-column", which is useless.
 (global-set-key (kbd "C-x f") 'djcb-find-file-as-root)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(send-mail-function (quote smtpmail-send-it)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 (setq ediff-split-window-function 'split-window-horizontally)
 
