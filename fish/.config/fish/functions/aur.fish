6cec32a9ecc6a2909867a6595fa0325d679678df
Fabian Homborg <FHomborg@gmail.com>
2015-10-13 00:22:06 +0200

    Update aur.fish, remove updatepkgs.fish
    Unfortunately, this is starting to look like a real AUR helper

--- fish/.config/fish/functions/aur.fish
+++ fish/.config/fish/functions/aur.fish
@@ -1,16 +1,23 @@
 function aur --description 'Quite possibly the stupidest aur helper ever invented'
-	# TODO: This does not handle dependencies - they don't seem to be exported via the json interface
 	# TODO: This does not correctly handle searches with multiple arguments
-	# TODO: Unhardcode ~/dev/build/{current,new}
-	# TODO: Implement "demote"
-	# TODO: Implement "build"
-	# There seems to be no way to do this
+	# TODO: Implement "demote" (move from current to new)
+	# TODO: Implement "build" (makepkg, possibly with dependencies)
+	set -q aurqueue; or set -l aurqueue ~/dev/build/new
+	set -q aurpkgs; or set -q aurpkgs ~/dev/build/current
+	set -l mode search
+	set -l yellow ""
+	set -l blue ""
+	set -l normal ""
+	if isatty stdout
+		set yellow (set_color yellow)
+		set normal (set_color normal)
+		set blue (set_color blue)
+	end
+
 	switch $argv[1]
-		case "search" "clone" "promote" "demote" "rm"
+		case "search" "clone" "promote" "demote" "rm" "update" # TODO: "build"
 			set mode $argv[1]
 			set -e argv[1]
-		case "*"
-			set mode search
 	end
 			
 	if not set -q argv[1]
@@ -19,41 +26,60 @@ function aur --description 'Quite possibly the stupidest aur helper ever invente
 	end
 
 	if contains -- $mode "search" "clone"
-		set -l tmp (curl -G --data-urlencode "arg=$argv" "https://aur.archlinux.org/rpc.php?type=search" -s)
-		set -l names (echo $tmp | jshon -e results -a -e Name -u)
-		set -l urls "https://aur.archlinux.org/"(echo $tmp | jshon -e results -a -e PackageBase -u)".git"
-		set -l descs (echo $tmp | jshon -e results -a -e Description -u)
 		if [ $mode = search ]
+			set -l arg
+			for a in $argv
+				set arg --data-urlencode "arg=$a" $arg
+			end
+			# This line dominates the profile, the jshon calls don't matter
+			set -l tmp (curl -G $arg "https://aur.archlinux.org/rpc.php?v=4&type=search" -s)
+			if [ (echo $tmp | jshon -e resultcount) -eq "0" ]
+				echo "No results found"
+				return 1
+			end
+			set -l names (echo $tmp | jshon -e results -a -e Name -u)
+			set -l descs (echo $tmp | jshon -e results -a -e Description -u)
+			set -l urls (echo $tmp | jshon -e results -a -e URL -u)
+			set -l versions (echo $tmp | jshon -e results -a -e Version -u)
 			for i in (seq (count $names))
-				printf "%-40s: %-60s (%s)\n" (set_color blue)$names[$i](set_color normal) (set_color normal)$urls[$i] (set_color yellow)$descs[$i](set_color normal)
+				printf "%s v%s\n\t%s (%s)\n" $blue$names[$i]$normal $versions[$i] $descs[$i] $yellow$urls[$i]$normal
 			end
 		else if [ $mode = clone ]
+			# For some reason only explicitly encoding the version gets us deps
+			set -l tmp (curl -G --data-urlencode "arg=$argv" "https://aur.archlinux.org/rpc.php?v=4&type=multiinfo" -s)
+			set -l names (echo $tmp | jshon -e results -a -e Name -u)
+			set -l descs (echo $tmp | jshon -e results -a -e Description -u)
+			set -l urls (echo $tmp | jshon -e results -a -e URL -u)
+			set -l versions (echo $tmp | jshon -e results -a -e Version -u)
+			set -l deps (echo $tmp | jshon -e results -a -e Depends -a -u -Q | sort -u)
+			set -l makedeps (echo $tmp | jshon -e results -a -e MakeDepends -a -u -Q | sort -u)
+			# set -l aurdeps (pacman -Si (string replace -ar '[>=<].*$' '' -- $deps $makedeps) >/dev/null ^| cut -d"'" -f2)
+			# This should handle versioned deps and providers
+			set -l aurdeps (pacman -Spq -- $deps $makedeps >/dev/null ^| string replace -r '^.*: ' '')
+			set -l cloneurls "https://aur.archlinux.org/"(echo $tmp | jshon -e results -a -e PackageBase -u)".git"
 			set -l clonenum 1
-			if set -q names[2]
-				for i in (seq (count $names))
-					printf "%d - %-40s: %s\n" $i (set_color blue)$names[$i] (set_color yellow)$descs[$i](set_color normal)
-				end
-				echo "Which one would you like to clone, master?"
-				read clonenum
+			echo "Aurdeps: $aurdeps"
+			for pkg in $aurdeps
+				echo "Now cloning: $pkg"
+				aur clone (string replace -ar '[>=<].*$' '' -- $pkg)
 			end
-			# Allow clone ranges
-			set clonenum (string split ".." -- $clonenum)
-			if set -q clonenum[2]
-				set clonenum (seq $clonenum[1] $clonenum[2])
-			end
-			for i in $clonenum
-				git clone $urls[$i] ~/dev/build/new/$names[$i]
-			end
+			git clone $cloneurls[$clonenum] $aurqueue/$names
 		end
 		return 0
 	else if [ $mode = "rm" ]
-		rm -rf ~/dev/build/new/$argv
+		rm -rf $aurqueue/$argv
 	else if [ $mode = "promote" ]
-		mv ~/dev/build/new/$argv ~/dev/build/current
+		mv $aurqueue/$argv $aurpkgs
 		for pkg in $argv
-			git -C ~/dev/build/current submodule add ./$pkg
+			git -C $aurpkgs submodule add ./$pkg
 		end
 	else if [ $mode = "demote" ]
 		echo "Demote mode is not implemented yet"
+	# TODO:
+	# else if [ $mode = "build" ]
+	# 	# First clone, then recursively build AUR deps, then build
+	else if [ $mode = "update" ]
+		git -C $aurpkgs submodule foreach git pull origin master
+		makepkgs $aurpkgs
 	end
 end
--- fish/.config/fish/functions/updatepkgs.fish
+++
@@ -1,5 +0,0 @@
-function updatepkgs
-	cd ~/dev/build/current
-	git submodule foreach git pull -q origin master
-	makepkgs *
-end
