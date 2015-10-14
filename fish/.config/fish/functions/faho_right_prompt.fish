f8a336cee498b8c811acb4500baa76cb8963f37e
Fabian Homborg <FHomborg@gmail.com>
2015-10-14 15:40:42 +0200

    fish: Remove faho_*_prompt
    My fish_*_prompt are tracked with git, so there's no need for these

--- fish/.config/fish/functions/faho_right_prompt.fish
+++
@@ -1,9 +0,0 @@
-function fish_right_prompt
-	# set -l battery_percentage (upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | cut -d":" -f2 | cut -d"%" -f1)
-	# set -l battery (bar $battery_percentage)
-	set -l bat (battery)
-	set -l plug
-	set -q BATTERY_IS_PLUGGED; and set plug "⚡"
-	set -l d (date "+%R %A %F")
-	printf "%s%s %s" $plug $bat $d
-end
