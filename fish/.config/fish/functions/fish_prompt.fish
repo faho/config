47727084e76189de85f855fdbaea4891c0262451
Fabian Homborg <FHomborg@gmail.com>
2015-10-15 19:11:27 +0200

    fish: Only print user@hostname if in vm or SSH

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,30 +1,38 @@
 function fish_prompt --description 'Prompt anzeigen'
 	set -l last_status $status
 
-	set -g __fish_prompt_hostname (hostname)
-	set -g __fish_prompt_normal (set_color normal)
-	
-	set -l delim '➤'
+    set -g __fish_prompt_hostname (hostname)
+    set -g __fish_prompt_normal (set_color normal)
 
-	switch $USER
-		case root
-			if set -q fish_color_cwd_root
-				set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-			else
-				set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-			end
-		case '*'
-			set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-	end
+    set -l delim '➤'
 
-	set -l prompt_status
-	if test $last_status -ne 0
-		set -g __fish_prompt_status (set_color $fish_color_status)
-		set prompt_status "$__fish_prompt_status [$last_status]$__fish_prompt_normal"
-	end
+    switch $USER
+        case root
+            if set -q fish_color_cwd_root
+                set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+            else
+                set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+            end
+        case '*'
+            set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+    end
 
-	set -g __fish_prompt_user (set_color $fish_color_user)
-	set -g __fish_prompt_host (set_color $fish_color_host)
+    set -l prompt_status
+    if test $last_status -ne 0
+        set -g __fish_prompt_status (set_color $fish_color_status)
+        set prompt_status "$__fish_prompt_status [$last_status]$__fish_prompt_normal"
+    end
 
-	echo -n -s "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
+    set -l host
+    if begin set -q SSH_TTY
+            or begin type -q systemd-detect-virt
+                and systemd-detect-virt -q
+            end
+        end
+        set -g __fish_prompt_user (set_color $fish_color_user)
+        set -g __fish_prompt_host (set_color $fish_color_host)
+        set host "$__fish_prompt_user$USER$__fish_prompt_normal@$__fish_prompt_host$__fish_prompt_hostname$__fish_prompt_normal "
+    end
+
+    echo -n -s $host "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
 end
