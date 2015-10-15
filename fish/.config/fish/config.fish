5445f4e9a714c52e2c7549ff83bc7b01b92b7867
Fabian Homborg <FHomborg@gmail.com>
2015-10-15 19:09:26 +0200

    fish: Wrap most config in status --is-interactive

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -29,64 +29,73 @@ set -x LESSHISTFILE "-"
 
 set FISH_CLIPBOARD_CMD "cat"
 
-function rm --wraps "rm"
-    command rm -I $argv
-end
-
-function alsamixer --wraps "alsamixer"
-    command alsamixer -c0
-end
-
-abbr -a sc=systemctl
-abbr -a usc="systemctl --user"
-abbr -a pm="pulsemixer"
-
-#alias mu4e emacs --eval "'(mu4e)'"
-function mu4e
-    emacs --eval "(mu4e)" $argv
-end
-
-#alias abs $HOME/dev/abs-replacement/abs.sh
-function abs
-    ~/dev/abs-replacement/abs.sh $argv
-end
-
-#alias upo upower -i /org/freedesktop/UPower/devices/battery_BAT0
-function upo
-    upower -i /org/freedesktop/UPower/devices/battery_BAT0 $argv
-end
-
-function startnvidia --description "Switch to X server backed by the nvidia card"
-    # sudo systemctl stop bumblebeed display-manager
-    sudo modprobe -r bbswitch
-    # set -x LD_LIBRARY_PATH "/usr/lib/nvidia:/usr/lib32/nvidia:/usr/lib:/usr/lib32"
-    # startx -- -config xorg-nvidia.conf
-    sudo systemctl start prime@$USER.service
-end
-
-function startintel --description "Start X server backed by the intel card"
-    sudo systemctl start bumblebeed display-manager
-end
-
-function fish_user_key_bindings
-    bind \cv yank # paste from killring/clipboard
-    bind -k ic yank # insert key
-	bind \ev yank-pop
-end
-
 if status --is-interactive
     logo
+	function rm --wraps "rm"
+		command rm -I $argv
+	end
+
+	function alsamixer --wraps "alsamixer"
+		command alsamixer -c0
+	end
+
+	abbr -a sc=systemctl
+	abbr -a usc="systemctl --user"
+	abbr -a pm="pulsemixer"
+
+	#alias mu4e emacs --eval "'(mu4e)'"
+	function mu4e
+		emacs --eval "(mu4e)" $argv
+	end
+
+	#alias abs $HOME/dev/abs-replacement/abs.sh
+	function abs
+		~/dev/abs-replacement/abs.sh $argv
+	end
+
+	#alias upo upower -i /org/freedesktop/UPower/devices/battery_BAT0
+	function upo
+		upower -i /org/freedesktop/UPower/devices/battery_BAT0 $argv
+	end
+
+	function startnvidia --description "Switch to X server backed by the nvidia card"
+		# sudo systemctl stop bumblebeed display-manager
+		sudo modprobe -r bbswitch
+		# set -x LD_LIBRARY_PATH "/usr/lib/nvidia:/usr/lib32/nvidia:/usr/lib:/usr/lib32"
+		# startx -- -config xorg-nvidia.conf
+		sudo systemctl start prime@$USER.service
+	end
+
+	function startintel --description "Start X server backed by the intel card"
+		sudo systemctl start bumblebeed display-manager
+	end
+
+	function fish_user_key_bindings
+		bind \cv yank # paste from killring/clipboard
+		bind -k ic yank # insert key
+		bind \ev yank-pop
+	end
+
+	function erase_grep_options --on-variable GREP_OPTIONS --description "Delete GREP_OPTIONS if it is ever set"
+		if set -q GREP_OPTIONS
+			echo "SOMETHING TRIED TO SET GREP_OPTIONS to $GREP_OPTIONS!"
+			set -e GREP_OPTIONS
+		end
+	end
+
+	set -g fish_term24bit 1
+
+	# Add path for completions/functions intended to go upstream
+	set fish_function_path ~/.config/fish/test/functions $fish_function_path
+	set fish_complete_path ~/.config/fish/test/completions $fish_complete_path
+
+	set -g __fish_git_prompt_show_informative_status 0
+	set -g __fish_git_prompt_showupstream informative
+	set -g __fish_git_prompt_describe_style contains
+	set -g __fish_git_prompt_showcolorhints 0
+
+	# Assorted emacs ansi-term workarounds:
+	if string match -i "eterm*" -- $TERM >/dev/null
+		function fish_title; end
+	end
 end
-
-function erase_grep_options --on-variable GREP_OPTIONS --description "Delete GREP_OPTIONS if it is ever set"
-    if set -q GREP_OPTIONS
-        echo "SOMETHING TRIED TO SET GREP_OPTIONS to $GREP_OPTIONS!"
-        set -e GREP_OPTIONS
-    end
-end
-
-set -g fish_term24bit 1
-
-# Add path for completions/functions intended to go upstream
-set fish_function_path ~/.config/fish/test/functions $fish_function_path
-set fish_complete_path ~/.config/fish/test/completions $fish_complete_path
