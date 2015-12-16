314401b4fa773e8a3f1d555fe2df36e9f778d1f6
Fabian Homborg <FHomborg@gmail.com>
2015-12-16 18:40:35 +0100

    emacs: Remove inhibit-startup-echo-area-message
    This doesn't actually work - it would need to be set to the current
    username (hardcoded, it even works as a comment).
    
    See http://emacshorrors.com/posts/advertising-your-freedom.html.
    
    For now I've decided that disabling is not worth it - it only blinks up anyway.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -20,7 +20,6 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (setq inhibit-startup-message t
-      inhibit-startup-echo-area-message t
 	  inhibit-splash-screen t
 	  inhibit-startup-buffer-menu t ; Don't show buffer list when opening multiple files
 	  inhibit-default-init t ; Don't read default.el - I like my settings to be mine
