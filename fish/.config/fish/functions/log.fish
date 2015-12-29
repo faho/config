9e76886c2a1d972bc81798ea737e2b50c6a9c7cc
Fabian Homborg <FHomborg@gmail.com>
2015-12-29 16:10:42 +0100

    fish: Remove debug messages from `log`

--- fish/.config/fish/functions/log.fish
+++ fish/.config/fish/functions/log.fish
@@ -1,3 +1,6 @@
 function log
-	journalctl -b -f -n 1000
+	# Only messages from this boot,
+	# priority 0 (emerg) to 6 (info) (i.e. excluding 7 - debug because that's too wordy)
+	# And follow after showing the last 1000 lines
+	journalctl -b -f -n 1000 --priority 0..6
 end
