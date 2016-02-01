ec773247d9a5bfaeb6d942a11d5ab3cde85f4302
Fabian Homborg <FHomborg@gmail.com>
2016-02-01 17:56:23 +0100

    fish_right_prompt: Add mpc support

--- fish/.config/fish/functions/fish_right_prompt.fish
+++ fish/.config/fish/functions/fish_right_prompt.fish
@@ -1,8 +1,18 @@
 function fish_right_prompt
-	type -q __fish_vcs_prompt; and __fish_vcs_prompt
+	type -q __fish_vcs_prompt; and set -l vcs (__fish_vcs_prompt)
 	set -l bat (battery)
-	set -l plug
-	set -q BATTERY_IS_PLUGGED; and set plug "⚡"
-	set -l d (date "+%R")
-	printf "%s%s %s" $plug $bat $d
+	set -q BATTERY_IS_PLUGGED; and set -l plug "⚡"
+	set -l d (set_color brgrey)(date "+%R")(set_color normal)
+	set -l mpc
+	if type -q mpc
+		set -l note "♪"
+		if set mpc (mpc status ^/dev/null)
+			if set -q mpc[2]; and string match -q "[playing]*" -- $mpc[2]
+				set mpc (set_color brcyan)"$note$mpc[1]$note"(set_color normal)
+			else
+				set -e mpc
+			end
+		end
+	end
+	printf "%s %s %s%s %s" $mpc $vcs $plug $bat $d
 end
