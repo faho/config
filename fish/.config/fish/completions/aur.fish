6d6ac384ba32dd154475216826960142f2d566d7
Fabian Homborg <FHomborg@gmail.com>
2016-04-18 11:18:21 +0200

    Update aur.fish completions

--- fish/.config/fish/completions/aur.fish
+++ fish/.config/fish/completions/aur.fish
@@ -1,11 +1,8 @@
-set -l cmds search clone promote demote rm build update
-complete -c aur -f -n "not __fish_seen_subcommand_from $cmds" -a "search"
-complete -c aur -f -n "not __fish_seen_subcommand_from $cmds" -a "clone"
-complete -c aur -f -n "not __fish_seen_subcommand_from $cmds" -a "promote"
+set -l cmds search clone download promote demote rm build install update log
+complete -c aur -f -n "not __fish_seen_subcommand_from $cmds" -a "$cmds"
 complete -c aur -f -n "__fish_seen_subcommand_from promote" -a "(aur ls --queueonly)"
-complete -c aur -f -n "not __fish_seen_subcommand_from $cmds" -a "demote"
 complete -c aur -f -n "__fish_seen_subcommand_from demote" -a "(aur ls --pkgsonly)"
-complete -c aur -f -n "not __fish_seen_subcommand_from $cmds" -a "rm"
 complete -c aur -f -n "__fish_seen_subcommand_from rm" -a "(aur ls --queueonly)"
-complete -c aur -f -n "not __fish_seen_subcommand_from $cmds" -a "build"
-complete -c aur -f -n "not __fish_seen_subcommand_from $cmds" -a "update"
+complete -c aur -f -n "__fish_seen_subcommand_from build install" -a "(aur ls)"
+complete -c aur -f -n "__fish_seen_subcommand_from update" -a "(aur ls)"
+complete -c aur -f -n "__fish_seen_subcommand_from log" -a "(aur ls)"
