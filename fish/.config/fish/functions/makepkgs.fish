eeb76283394b0614ad19c488d42dc0d28d2f4430
Fabian Homborg <FHomborg@gmail.com>
2016-04-18 11:12:33 +0200

    Add dependency "support" to makepkgs
    This stops us from retrying so much when there's dependency issues.

--- fish/.config/fish/functions/makepkgs.fish
+++ fish/.config/fish/functions/makepkgs.fish
@@ -3,25 +3,49 @@ function makepkgs --description 'Build multiple packages with makepkg'
         echo "Please specify packages"
         return
     end
-    set -l built 0 # has a new package been built last run? (0=true, 1=false)
-    while [ $built -eq 0 -a (count $argv) -gt 0 ]
-        set -l built 1
-        for pkg in $argv
+
+	set -l opts
+	set -l packages
+	for arg in $argv
+		switch $arg
+			case "-*"
+				set opts $opts $arg
+			case "*"
+				set packages $packages $arg
+		end
+	end
+	# directories could be relative, so always return to the original directory
+	set -l startdir $PWD
+    set -l failed # packages that failed to build
+    while [ (count $packages) -gt 0 ]
+        for pkg in $packages
+			cd $startdir
             echo "Making $pkg"
-            pushd $pkg
-            # Install dependencies and the resulting package, but only when needed
-            # Don't remove dependencies because they might be needed by the next package
-            # Also log output and clean on successful build
-            makepkg -sic --needed
-            if [ $status -eq 0 ]
-                # Remove package that has been built successfully
-                set -e argv[(contains -i $pkg $argv)]
-                set built 0
-            end
-            popd
+            cd $pkg # directories could also be absolute, so this needs to be a separate cd
+			# Check .SRCINFO for dependencies
+			if set -l aurdeps (test -r .SRCINFO; or makepkg --printsrcinfo > .SRCINFO; faho_getaurdeps < .SRCINFO)
+				if not set -l missing (pacman -T -- $aurdeps)
+					if not set -q packages[2]
+						# Abort instead of skipping if we are the last pkg - or we'd loop endlessly
+						set failed $failed $pkg
+						set -e packages
+						break
+					else
+						# Skip because there's unresolvable deps (likely from the AUR)
+						# These might still be in the transaction
+						continue
+					end
+				end
+			end
+            # Install dependencies (-s) and the resulting package (-i), but only when needed (--needed)
+            # Don't remove dependencies because they might be needed by the next package (that would be -r)
+            # Also log output (-L) and clean on successful build (-c)
+            makepkg -sicL --needed; or set failed $failed $pkg
+            set -e packages[(contains -i $pkg $packages)]
         end
     end
-    if [ $built -gt 0 ]
-        echo "No new packages could be built, please check"
+    if [ (count $failed) -gt 0 ]
+        echo (count $failed) packages failed to build: $failed
     end
+	cd $startdir
 end
