6b4d5df15010ca3dca9ec372028116ccdf387099
Fabian Homborg <FHomborg@gmail.com>
2016-04-18 16:13:40 +0200

    fish_prompt: Shorten when it's too wide

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -15,15 +15,22 @@ function fish_prompt
     test $last_status -ne 0; and set -l prompt_status (set_color $fish_color_status)"[$last_status]$normal"
 
 	# Only show host if in SSH or container
-	# Cache this in a global variable
-	set -q prompt_host; or begin set -g prompt_host
+	# Store this in a global variable because it's slow and unchanging
+	set -q prompt_host; or begin set -g prompt_host ""
 		if set -q SSH_TTY
 			or begin type -q systemd-detect-virt
 				and systemd-detect-virt -q
 			end
-			set prompt_host $usercolor$USER$normal@(set_color $fish_color_host)(hostname)(set_color normal)":"
+			set prompt_host $usercolor$USER$normal@(set_color $fish_color_host)(hostname)$normal":"
 		end
 	end
 
-    echo -n -s $prompt_host $cwd (prompt_pwd) $normal $prompt_status $delim ' '
+	# Shorten pwd if prompt is too long
+	# TODO: This will break if prompt_host is actually used because of the control sequences
+	set -l pwd (prompt_pwd)
+	set -l len (string length -- $prompt_host$pwd$last_status$delim' ')
+	if test $len -gt $COLUMNS
+		set pwd (string replace -ar '([^/])[^/]*' '$1' -- $pwd)
+	end
+    echo -n -s $prompt_host $cwd $pwd $normal $prompt_status $delim ' '
 end
