a85461783005130d3186d580efdf5efeecdc5215
Fabian Homborg <FHomborg@gmail.com>
2016-05-29 16:34:00 +0200

    fish: Remove clipboard bindings
    These are now upstream.

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,7 +1,3 @@
 function fish_user_key_bindings
-    bind \cx "commandline | xsel --clipboard"
-    bind \cv "commandline -i (xsel --clipboard) ^/dev/null"
-    bind -k ic yank # insert key
-    bind \ev yank-pop
 	bind \cr 'fish_replace_regex'
 end
