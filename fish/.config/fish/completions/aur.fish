8c6e9a2f8798926f323f4643259514ff237c74ff
Fabian Homborg <FHomborg@gmail.com>
2016-08-31 18:02:54 +0200

    aur.fish: Add missing `show` to completions

--- fish/.config/fish/completions/aur.fish
+++ fish/.config/fish/completions/aur.fish
@@ -1,4 +1,4 @@
-set -l cmds search clone download promote demote rm build install update log
+set -l cmds search clone download promote demote rm build install update log show
 complete -c aur -f -n "not __fish_seen_subcommand_from $cmds" -a "$cmds"
 complete -c aur -f -n "__fish_seen_subcommand_from promote" -a "(aur ls --queueonly)"
 complete -c aur -f -n "__fish_seen_subcommand_from demote" -a "(aur ls --pkgsonly)"
@@ -6,3 +6,4 @@ complete -c aur -f -n "__fish_seen_subcommand_from rm" -a "(aur ls --queueonly)"
 complete -c aur -f -n "__fish_seen_subcommand_from build install" -a "(aur ls)"
 complete -c aur -f -n "__fish_seen_subcommand_from update" -a "(aur ls)"
 complete -c aur -f -n "__fish_seen_subcommand_from log" -a "(aur ls)"
+complete -c aur -f -n "__fish_seen_subcommand_from show" -a "(aur ls)"
