90a1176088c46a8adf698fdaa30366ef41fe3994
Fabian Homborg <FHomborg@gmail.com>
2017-01-06 20:34:36 +0100

    fish: Remove bracketed paste
    This doesn't currently help anything.

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,12 +1,3 @@
 function fish_user_key_bindings
 	functions -q fzf_key_bindings; and fzf_key_bindings
-    # In paste-mode, self-insert except for the sequence to get out of it
-    # This should return to the previous mode
-    bind -M paste -m default \e\[201~ force-repaint
-    bind -M paste "" self-insert
-    # Without this, a \r will overwrite the other text, rendering it invisible - which makes the exercise kinda pointless.
-    bind -M paste \r "commandline -i \n"
-    bind -M default -m paste \e\[200~ ''
-    # Enable bracketed paste mode
-    printf "\e[?2004h"
 end
