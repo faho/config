a21048a05ac00cbb014c1e6b6bcbb1dc3d02de26
Fabian Homborg <FHomborg@gmail.com>
2017-03-09 20:16:44 +0100

    psgrep: Use pgrep -af

--- fish/.config/fish/functions/psgrep.fish
+++ fish/.config/fish/functions/psgrep.fish
@@ -1,9 +1,8 @@
 function psgrep
 	set -l ret 1
     for i in $argv
-	    if set -l p (pgrep -f $i)
+	    if pgrep -af $i
             set ret 0
-	        ps up $p
         end
     end
     return $ret
