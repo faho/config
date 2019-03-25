fc70b724205bd22c5a5d1feb701e3a5b855d6761
Fabian Homborg <FHomborg@gmail.com>
2019-03-25 17:43:38 +0100

    mkfish: Export the branch
    My copy of the fish-git PKGBUILD uses "$branch" if it exists, so we
    can just export it instead of modifying the PKGBUILD.
    
    Now if only I could stop makepkg from doing that...

--- fish/.config/fish/functions/mkfish.fish
+++ fish/.config/fish/functions/mkfish.fish
@@ -1,5 +1,5 @@
-function mkfish
-	set -l branch master
+function mkfish --wraps makepkg
+	set -lx branch master
 	set -l opts
 	for arg in $argv
 		switch $arg
@@ -10,6 +10,5 @@ function mkfish
 		end
 	end
 	cd ~/dev/build/fish-shell-git
-	sed -e "s|source=.*|source=('git+file:///home/alfa/dev/fish-shell#branch=$branch')|" -i PKGBUILD
 	makepkg -sir $opts
 end
