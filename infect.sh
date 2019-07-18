3595a18bf3e6bd8e314d4731957086781c36a87a
Fabian Homborg <FHomborg@gmail.com>
2019-07-18 19:18:12 +0200

    Posixify infect.bash

--- infect.sh
+++ infect.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-if ! which stow > /dev/null 2>&1 ; then
+#!/bin/sh
+if ! command -v stow > /dev/null 2>&1 ; then
     echo "Install gnu stow"
     exit 1
 fi
 
 for d in *; do
-    [[ ! -d "$d" ]] && continue
+    [ ! -d "$d" ] && continue
     stow -S -t "$HOME" "$d"
 done
