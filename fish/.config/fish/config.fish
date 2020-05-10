8bb35685615d80bf81a99b3307cdac4ab03cc107
Fabian Homborg <FHomborg@gmail.com>
2020-05-10 19:32:41 +0200

    fish: Add XDG_DATA_DIRS
    This is otherwise set via /etc/profile.d on arch.
    
    Blergh

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,9 +3,10 @@
 #
 
 # These go first, because other stuff depends on them.
-set -gx XDG_CACHE_HOME "$HOME/.cache"
-set -gx XDG_CONFIG_HOME "$HOME/.config"
-set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx --path XDG_DATA_DIRS $XDG_DATA_HOME/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
 
 set -gx ANDROID_HOME ~/.android # /opt/android-sdk
 set -gx ASPROOT ~/packages/asp
