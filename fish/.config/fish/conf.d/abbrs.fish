0318d62d8cf90ddefd45255a505ac95f9c4550c8
Fabian Homborg <FHomborg@gmail.com>
2020-05-23 12:36:41 +0200

    fish: reindent

--- fish/.config/fish/conf.d/abbrs.fish
+++ fish/.config/fish/conf.d/abbrs.fish
@@ -1,17 +1,17 @@
 if not set -q fish_initialized
-	abbr -a alsamixer alsamixer -c0
-	abbr -a e emacs -nw
-	abbr -a \$PAGER less
-	# abbr -a mu4e emacs --eval "\(mu4e\)"
-	abbr -a pm pulsemixer
-	abbr -a rm rm -I
-	abbr -a sc systemctl
-	abbr -a upo upower -i /org/freedesktop/UPower/devices/battery_BAT0
-	abbr -a usc systemctl --user
-	# Double-escaping needed
-	abbr -a d2 env WINEPREFIX=/home/alfa/.wine32/ wine ~/.wine/drive_c/Program\\ Files\\ \\(x86\\)/Diablo\\ II/Diablo\\ II.exe
+    abbr -a alsamixer alsamixer -c0
+    abbr -a e emacs -nw
+    abbr -a \$PAGER less
+    # abbr -a mu4e emacs --eval "\(mu4e\)"
+    abbr -a pm pulsemixer
+    abbr -a rm rm -I
+    abbr -a sc systemctl
+    abbr -a upo upower -i /org/freedesktop/UPower/devices/battery_BAT0
+    abbr -a usc systemctl --user
+    # Double-escaping needed
+    abbr -a d2 env WINEPREFIX=/home/alfa/.wine32/ wine ~/.wine/drive_c/Program\\ Files\\ \\(x86\\)/Diablo\\ II/Diablo\\ II.exe
     # curl requires one "-O" per argument, but doesn't complain about getting too many.
-	abbr -a c curl -LOOOOOOO -C -
-	# set -U  fish_user_paths ~alfa/.local/bin $GOPATH/bin
-	set -U fish_initialized
+    abbr -a c curl -LOOOOOOO -C -
+    # set -U  fish_user_paths ~alfa/.local/bin $GOPATH/bin
+    set -U fish_initialized
 end
--- fish/.config/fish/conf.d/colors.fish
+++ fish/.config/fish/conf.d/colors.fish
@@ -1,39 +1,39 @@
 if not set -q faho_colors_set
-	echo Setting colors
-	set -U faho_colors_set
-	set -U fish_color_normal B2B2B2 # normal
-	set -U fish_color_command 005fd7
-	set -U fish_color_param 00afff # cyan
-	set -U fish_color_redirection FFF # normal
-	set -U fish_color_comment 666 # THE NUMBER OF THE BEAST...
-	set -U fish_color_error A00 --bold
-	set -U fish_color_escape 0AA # cyan
-	set -U fish_color_operator 0AA # cyan
-	set -U fish_color_quote A50 # brown
-	set -U fish_color_autosuggestion 555 # yellow
-	set -U fish_color_valid_path --underline
-	set -U fish_color_cwd 0A0 # green
-	set -U fish_color_cwd_root A00 # red
+    echo Setting colors
+    set -U faho_colors_set
+    set -U fish_color_normal B2B2B2 # normal
+    set -U fish_color_command 005fd7
+    set -U fish_color_param 00afff # cyan
+    set -U fish_color_redirection FFF # normal
+    set -U fish_color_comment 666 # THE NUMBER OF THE BEAST...
+    set -U fish_color_error A00 --bold
+    set -U fish_color_escape 0AA # cyan
+    set -U fish_color_operator 0AA # cyan
+    set -U fish_color_quote A50 # brown
+    set -U fish_color_autosuggestion 555 # yellow
+    set -U fish_color_valid_path --underline
+    set -U fish_color_cwd 0A0 # green
+    set -U fish_color_cwd_root A00 # red
 
     set -U fish_color_status --background=red white
-	# Background color for matching quotes and parenthesis
-	set -U fish_color_match 0AA # cyan
+    # Background color for matching quotes and parenthesis
+    set -U fish_color_match 0AA # cyan
 
-	# Background color for search matches
-	set -U fish_color_search_match --background=533 # --background=purple
+    # Background color for search matches
+    set -U fish_color_search_match --background=533 # --background=purple
 
-	# Background color for selections
-	set -U fish_color_selection --background=B218B2 # --background=purple
+    # Background color for selections
+    set -U fish_color_selection --background=B218B2 # --background=purple
 
-	# Pager colors
-	set -U fish_pager_color_prefix --underline 0AA # cyan
-	set -U fish_pager_color_completion $fish_color_normal
-	set -U fish_pager_color_description $fish_color_comment
-	set -U fish_pager_color_progress $fish_color_match
+    # Pager colors
+    set -U fish_pager_color_prefix --underline 0AA # cyan
+    set -U fish_pager_color_completion $fish_color_normal
+    set -U fish_pager_color_description $fish_color_comment
+    set -U fish_pager_color_progress $fish_color_match
 
-	#
-	# Directory history colors
-	#
+    #
+    # Directory history colors
+    #
 
-	set -U fish_color_history_current 0AA # cyan
+    set -U fish_color_history_current 0AA # cyan
 end
