63250ddbb7aa15953b4fd150af0fa95b8e91bf4a
Fabian Homborg <FHomborg@gmail.com>
2021-08-29 08:48:23 +0200

    mkfish: Allow for non-branch fragments
    Given like e.g. '#tag=v2.1'

--- fish/.config/fish/functions/mkfish.fish
+++ fish/.config/fish/functions/mkfish.fish
@@ -1,15 +1,16 @@
-function mkfish --wraps makepkg
-    set -lx branch master
+function mkfish --wraps=makepkg
+    set -lx branch "#branch=master"
     set -l opts
     for arg in $argv
         switch $arg
             case "-*"
                 set opts $opts $arg
-            case "*"
+            case "#*"
                 set branch $arg
+            case "*"
+                set branch "#branch=$arg"
         end
     end
     cd ~/dev/build/fish-shell-git
-    set branch "#branch=$branch"
     makepkg -sir $opts
 end
