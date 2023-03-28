bd3fdf2de8fa4cedc6e35c7a1b1f81a5fa9edb4c
Fabian Boehm <FHomborg@gmail.com>
2023-03-28 17:55:44 +0200

    Update fish prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -4,6 +4,16 @@ function fish_prompt
     set -l normal (set_color normal)
     set -l usercolor (set_color $fish_color_user)
 
+    set -g __fish_git_prompt_showdirtystate 1
+    # set -g __fish_git_prompt_showuntrackedfiles 1
+    set -g __fish_git_prompt_showupstream informative
+    set -g __fish_git_prompt_showcolorhints 1
+    set -g __fish_git_prompt_use_informative_chars 1
+    # Unfortunately this only works if we have a sensible locale
+    string match -qi "*.utf-8" -- $LANG $LC_CTYPE $LC_ALL
+    and set -g __fish_git_prompt_char_dirtystate \U1F4a9
+    set -g __fish_git_prompt_char_untrackedfiles "?"
+
     set -l delim \U25BA
     # If we don't have unicode use a simpler delimiter
     string match -qi "*.utf-8" -- $LANG $LC_CTYPE $LC_ALL; or set delim ">"
@@ -11,12 +21,31 @@ function fish_prompt
     fish_is_root_user; and set delim "#"
 
     set -l cwd (set_color $fish_color_cwd)
-    if command -sq sha256sum
+    if command -sq cksum
         # randomized cwd color
-        set -l shas (pwd -P | sha256sum | string sub -l 6 | string match -ra ..)
-        # Increase color a bit so we don't get super dark colors.
-        # Really we want some contrast to the background (assuming black).
-        set -l col (for f in $shas; math --base=hex "min(255, 0x$f + 0x30)"; end | string replace 0x '' | string pad -c 0 -w 2 | string join "")
+        # We hash the physical PWD and turn that into a color. That means directories (usually) get different colors,
+        # but every directory always gets the same color. It's deterministic.
+        # We use cksum because 1. it's fast, 2. it's in POSIX, so it should be available everywhere.
+        # set -l shas (math --base=hex "($(pwd -P | string escape --style=hex | string sub -s -5000 | string replace -ra '(...?.?.?.?)' '0x$1$1$1\n' | string join -n +)) % 0xffffff" | string sub -s 3 | string pad -c 0 -w 6 | string match -ra ..)
+        set -l shas (pwd -P | cksum | string split -f1 ' ' | math --base=hex | string sub -s 3 | string pad -c 0 -w 6 | string match -ra ..)
+        set -l col 0x$shas[1..3]
+
+        # If the (simplified idea of) luminance is below 120 (out of 255), add some more.
+        # (this runs at most twice because we add 60)
+        set -l inds 1 2 3 1 2 3 1 2 3
+        if test $col[1] -gt $col[2]
+            test $col[1] -le $col[3]
+            and set -e inds[1..2]
+        else
+            test $col[2] -le $col[3]
+            and set -e inds[1]
+            or set -e inds[1..2]
+        end
+        while test (math 0.2126 x $col[1] + 0.7152 x $col[2] + 0.0722 x $col[3]) -lt 120
+            set col[$inds[1]] (math --base=hex "max 0, min(255, $col[$inds[1]] x 1.1 + 80)")
+            set -e inds[1]
+        end
+        set -l col (string replace 0x '' $col | string pad -c 0 -w 2 | string join "")
 
         set cwd (set_color $col)
     end
@@ -29,31 +58,31 @@ function fish_prompt
     # Store this in a global variable because it's slow and unchanging
     if not set -q prompt_host
         set -g prompt_host ""
-        set -g prompt_host_nocolor ""
         if set -q SSH_TTY
             or begin
                 command -sq systemd-detect-virt
                 and systemd-detect-virt -q
             end
-            set -l host (hostname)
-            set prompt_host_nocolor $USER@$host
-            set prompt_host $usercolor$USER$normal@(set_color $fish_color_host)$host$normal":"
+            set prompt_host $usercolor$USER$normal@(set_color $fish_color_host)$hostname$normal":"
         end
     end
 
     # Shorten pwd if prompt is too long
-    set -l pwd (prompt_pwd)
-    # This is quite cheesy - we simply try all prompt_pwds until it fits.
-    # Since prompt_pwd is builtins-only, this is usually quite fast.
-    # 0 means unshortened
-    set -l col (math $COLUMNS - 10)
-    for i in $fish_prompt_pwd_dir_length 0 5 4 3 2 1
-        set pwd (fish_prompt_pwd_dir_length=$i prompt_pwd)
-        set -l len (string length -- $prompt_host_nocolor$pwd$last_status$delim' ')
-        if test $len -lt $col
-            break
+    set -l pwd
+    # TODO: hg doesn't work here because we need that cdup,
+    # or we'd run into problems with logical vs physical paths.
+    if set -l vcs (git rev-parse --show-toplevel --show-prefix 2>/dev/null)
+        if set -l pre (string replace -- $vcs[2] '' $PWD/); or test -z "$vcs[2]"
+            set pwd (string join "" -- (prompt_pwd -d1 -D1 (string trim -rc / -- $pre)) \
+                $normal(fish_vcs_prompt '[%s]') \
+                $cwd/(prompt_pwd -- (string trim -rc / -- $vcs[2])))
+        else
+            set pwd (prompt_pwd) $normal(fish_vcs_prompt '(%s)')
         end
+    else
+        set pwd (prompt_pwd)
     end
 
+    # echo -n -s $delim
     echo -n -s $prompt_host $cwd $pwd $normal $prompt_status $delim
 end
--- fish/.config/fish/functions/fish_right_prompt.fish
+++ fish/.config/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,17 @@
 function fish_right_prompt
-    set -l vcs (fish_vcs_prompt 2>/dev/null)
+    set -g __fish_git_prompt_showdirtystate 1
+    # set -g __fish_git_prompt_showuntrackedfiles 1
+    set -g __fish_git_prompt_showupstream informative
+    set -g __fish_git_prompt_showcolorhints 1
+    set -g __fish_git_prompt_use_informative_chars 1
+    # Unfortunately this only works if we have a sensible locale
+    string match -qi "*.utf-8" -- $LANG $LC_CTYPE $LC_ALL
+    and set -g __fish_git_prompt_char_dirtystate \U1F4a9
+    set -g __fish_git_prompt_char_untrackedfiles "?"
+
+    # The git prompt's default format is ' (%s)'.
+    # We don't want the leading space.
+    # set -l vcs (fish_vcs_prompt '(%s)' 2>/dev/null)
 
     set -l d (set_color brgrey)(date "+%R")(set_color normal)
 
@@ -10,16 +22,11 @@ function fish_right_prompt
         set duration
     end
 
-    set -l shlvl ""
-    if test "$SHLVL" -ge 2
-        set shlvl "SUBSHELL $SHLVL"
-    end
-
     set -q VIRTUAL_ENV_DISABLE_PROMPT
     or set -g VIRTUAL_ENV_DISABLE_PROMPT true
     set -q VIRTUAL_ENV
     and set -l venv (string replace -r '.*/' '' -- "$VIRTUAL_ENV")
 
-    set_color reset
-    string join " " -- $shlvl $venv $duration $vcs $d
+    set_color normal
+    string join " " -- $venv $duration $vcs $d
 end
